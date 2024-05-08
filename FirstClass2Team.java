5893fbb83969376e84c08fc460ada419d99d0c94
benjamin474 <coding47414@gmail.com>
2024-05-08 15:20:24 +0800

    Init

---
+++ FirstClass2Team.java
@@ -0,0 +1,7 @@
+package ntou.cs.java2024;
+public class FirstClass2Team{
+    public static void main(String[] args){
+        System.out.println("這是用來練習Github協作的程式!");
+        System.out.println("學號：\"01157008\"\n姓名：\"蔡秉杰\"");
+    }
+}
