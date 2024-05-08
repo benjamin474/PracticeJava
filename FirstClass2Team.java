a881c7c398632763519899048c47f32a0724f573
AnthonyQwO <anthonyaa0423@gmail.com>
2024-05-08 15:27:04 +0800

    Update FirstClass.

--- FirstClass2Team.java
+++ FirstClass2Team.java
@@ -3,5 +3,6 @@ public class FirstClass2Team{
     public static void main(String[] args){
         System.out.println("這是用來練習Github協作的程式!");
         System.out.println("學號：\"01157008\"\n姓名：\"蔡秉杰\"");
+        System.out.println("學號：\"01157045\"\n姓名：\"唐予安\"");
     }
 }
